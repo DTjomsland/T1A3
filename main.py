7e46851fcdcfd063dfcdb540fd189d10076e6636
DTjomsland <dtjomslandjr@gmail.com>
2022-07-10 19:29:18 +0800

    Created dictionaries for druid and hunter stat weights

---
+++ main.py
@@ -0,0 +1,36 @@
+feral_druid = {
+    "strength": 2.27,
+    "agility": 3.61,
+    "intellect": 0.38,
+    "stamina": 0.17,
+    "hit": 1.00,
+    "expertise": 3.28,
+    "attack power": 1.00,
+    "crit": 2.54,
+    "haste": 1.53,
+    "armor penetration": 0.47,
+}
+
+balance_druid = {
+    "intellect": 2.27,
+    "spirit": 3.61,
+    "spell damage": 0.38,
+    "arcane damage": 0.17,
+    "nature damage": 1.00,
+    "spell hit": 3.28,
+    "spell crit": 1.00,
+    "spell haste": 2.54,
+    "mp5": 1.53,
+}
+
+hunter = {
+    "strength": 0.15,
+    "agility": 2.5,
+    "intellect": 0.01,
+    "stamina": 0.17,
+    "hit": 3.57,
+    "attack power": 1.00,
+    "crit": 1.78,
+    "haste": 1.58,
+    "armor penetration": 0.36,
+}
