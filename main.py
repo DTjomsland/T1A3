d4528480d71b02ceab40a11b208ed980522f4716
DTjomsland <dtjomslandjr@gmail.com>
2022-07-11 21:15:53 +0800

    Added classes and error handling for items

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import class_info
 def _get_player_class():
-    print('Please pick one of the following classes:')
-    print(*list(classes.keys()), sep="\t")
+    print('Please pick one of the following classes: ')
+    print(*list(class_info.classes.keys()), sep="   ")
     player_class = input('Your Class: ').title()
-    while player_class not in list(classes.keys()):
+    while player_class not in list(class_info.classes.keys()):
         player_class = input('Please choose a valid class: ').title()
     return player_class
 
 def _get_specialization(player_class):
     print('Please pick one of the following specializations:')
-    print(*list(classes[player_class].keys()), sep="\t")
+    print(*list(class_info.classes[player_class].keys()), sep="   ")
     specialization = input('Your specialization: ').title()
-    while specialization not in list(classes[player_class].keys()):
+    while specialization not in list(class_info.classes[player_class].keys()):
         specialization = input('Please choose a valid specialization:').title()
     return specialization
 
 def _get_item(player_class, specialization, number):
     item = {}
     print(f"Please enter stat values for Item {number}: ")
-    for k, v in classes[player_class][specialization].items():
+    for k, v in class_info.classes[player_class][specialization].items():
         item[k] = None   
     for k, v in item.items():
-        item[k] = int(input(str(k).title() + ": "))
+        while True:
+            try:
+                item[k] = float(input(str(k).title() + ": "))
+            except ValueError:
+                print('Please enter a numerical value.')
+            else:
+                break
     return item
 
 def _calculate_stats(player_class, specialization, item):
     item_calc = item
     for k, v in item_calc.items():
-        if k in classes[player_class][specialization]:
-            item_calc[k] = v * classes[player_class][specialization][k]
+        if k in class_info.classes[player_class][specialization]:
+            item_calc[k] = v * class_info.classes[player_class][specialization][k]
     return item_calc
 
 def _compare_items(item1_calc, item2_calc):
@@ -48,8 +54,15 @@ def main_function():
     item2_calc = _calculate_stats(player_class, specialization, item2)
     _compare_items(item1_calc, item2_calc)
 
-main_function()  
+def restart():
+    restart = input("Would you like to restart this program? ")
+    if restart == "yes" or restart == "y":
+        main_function()
+    if restart == "n" or restart == "no":
+        print("Script terminating. Goodbye.")
 
+main_function()  
+restart()
 
 
 
