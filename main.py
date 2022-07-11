6912c60b0be971e361042274d9c11e9bb585c5b7
DTjomsland <dtjomslandjr@gmail.com>
2022-07-11 10:26:21 +0800

    created all dps spec dictionaries

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-feral_druid = {
+druid = {
     "strength": 2.27,
     "agility": 3.61,
     "intellect": 0.38,
@@ -9,9 +9,6 @@ feral_druid = {
     "crit": 2.54,
     "haste": 1.53,
     "armor penetration": 0.47,
-}
-
-balance_druid = {
     "intellect": 2.27,
     "spirit": 3.61,
     "spell damage": 0.38,
@@ -33,4 +30,92 @@ hunter = {
     "crit": 1.78,
     "haste": 1.58,
     "armor penetration": 0.36,
+}
+
+mage = {
+    "intellect": 1.57,
+    "spirit": 0.99,
+    "spell damage": 1.00,
+    "arcane damage": 0.78,
+    "spell hit": 1.38,
+    "spell crit": 0.83,
+    "spell haste": 0.77,
+    "mp5": 0.59,
+}
+
+paladin = {
+    "strength": 2.77,
+    "agility": 2.05,
+    "intellect": 0.38,
+    "stamina": 0.17,
+    "hit": 0.53,
+    "expertise": 5.65,
+    "attack power": 1.00,
+    "crit": 2.15,
+    "haste": 2.55,
+    "armor penetration": 0.27,
+}
+
+priest = {
+    "intellect": 0.09,
+    "spirit": 0.11,
+    "spell damage": 1.00,
+    "shadow damage": 0.96,
+    "spell hit": 0.00,
+    "spell crit": 0.18,
+    "spell haste": 0.90,
+}
+
+rogue = {
+    "strength": 1.10,
+    "agility": 2.23,
+    "hit": 2.55,
+    "expertise": 3.43,
+    "attack power": 1.00,
+    "crit": 1.82,
+    "haste": 2.32,
+    "armor penetration": 0.34,
+}
+
+shaman = {
+    "strength": 2.20,
+    "agility": 1.69,
+    "intellect": 0.03,
+    "stamina": 0.17,
+    "hit": 1.00,
+    "expertise": 2.81,
+    "attack power": 1.00,
+    "crit": 1.68,
+    "haste": 1.50,
+    "armor penetration": 0.27,
+    "intellect": 2.27,
+    "spirit": 3.61,
+    "spell damage": 1.00,
+    "arcane damage": 0.17,
+    "nature damage": 1.00,
+    "spell hit": 1.80,
+    "spell crit": 1.00,
+    "spell haste": 1.33,
+    "mp5": 0.34,
+}
+
+priest = {
+    "intellect": 0.39,
+    "spell damage": 1.00,
+    "shadow damage": 0.93,
+    "spell hit": 2.14,
+    "spell crit": 0.69,
+    "spell haste": 0.96,
+    "mp5": 0.23,
+}
+
+warrior = {
+    "strength": 1.79,
+    "agility": 1.01,
+    "hit": 0.97,
+    "expertise": 2.02,
+    "attack power": 1.00,
+    "crit": 1.31,
+    "haste": 1.54,
+    "armor penetration": 0.29,
 }
