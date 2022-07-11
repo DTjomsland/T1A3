36ef1028dbfa6f0cf8374c7d0799f3a7578684aa
DTjomsland <dtjomslandjr@gmail.com>
2022-07-11 18:43:33 +0800

    get item 1 stats

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-def _get_player_class():
-    print('Please pick one of the following classes:')
-    print(*list(classes.keys()), sep="\t")
-    player_class = input('Your Class: ').title()
-    while player_class not in list(classes.keys()):
-        player_class = input('Please choose a valid class: ').title()
-    return player_class
-
-def _get_specialization(player_class):
-    print('Please pick one of the following specializations:')
-    print(*list(classes[player_class].keys()), sep="\t")
-    specialization = input('Your specialization: ').title()
-    while specialization not in list(classes[player_class].keys()):
-        specialization = input('Please choose a valid specialization:').title()
-    return specialization
-
-def main_function():
-    player_class = _get_player_class()
-    specialization = _get_specialization(player_class)
-    # item1 = _get_item(player_class, specialization, 1)
-    # item2 = _get_item(player_class, specialization, 2)
-    # item1_calc = _calculate_stats(player_class, specialization, item1)
-    # item2_calc = _calculate_stats(player_class, specialization, item2)
-    # _compare_items(item1_calc, item2_calc)
-
-main_function()  
-
-
-
 classes = {
     "Warlock": {
          "Demonology": {
@@ -68,6 +39,43 @@ classes = {
         }
     }
 }
+
+
+def _get_player_class():
+    print('Please pick one of the following classes:')
+    print(*list(classes.keys()), sep="\t")
+    player_class = input('Your Class: ').title()
+    while player_class not in list(classes.keys()):
+        player_class = input('Please choose a valid class: ').title()
+    return player_class
+
+def _get_specialization(player_class):
+    print('Please pick one of the following specializations:')
+    print(*list(classes[player_class].keys()), sep="\t")
+    specialization = input('Your specialization: ').title()
+    while specialization not in list(classes[player_class].keys()):
+        specialization = input('Please choose a valid specialization:').title()
+    return specialization
+
+def _get_item(player_class, specialization, number):
+    item = {}
+    print(f"Please enter stat values for Item {number}: ")
+    for k, v in classes[player_class][specialization].items():
+        item[k] = None   
+    for k, v in item.items():
+        item[k] = int(input(str(k).title() + ": "))
+    return item
+
+def main_function():
+    player_class = _get_player_class()
+    specialization = _get_specialization(player_class)
+    item1 = _get_item(player_class, specialization, 1)
+
+main_function()  
+
+
+
+
 
 
 
