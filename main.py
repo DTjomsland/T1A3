16f4931f35d44a080175a248d9b7855ce7be5c90
David Tjomsland <davidtjomsland@192.168.20.13>
2022-07-12 17:08:38 +0800

    added styling to program

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import class_info
-
+from colorama import Fore, Back, Style
 from art import *
+from playsound import playsound
 
-print(text2art('''Dave's TBC Gear Tool''', font="small"))
+print(Fore.MAGENTA + text2art('''Dave's TBC Gear Tool''', font="small") + Style.RESET_ALL)
 
 def _get_player_class():
-    print('Please pick one of the following classes: ')
+    print(Fore.CYAN + 'Please choose one of the following classes: \n' + Style.RESET_ALL)
     print(*list(class_info.classes.keys()), sep="   ")
+    print()
     player_class = input('Your Class: ').title()
+    print()
     while player_class not in list(class_info.classes.keys()):
-        player_class = input('Please choose a valid class: ').title()
+        print(Fore.YELLOW + 'Please choose a valid class. \n'+ Style.RESET_ALL)
+        player_class = input('Your Class: ').title()
+        print()
     return player_class
 
 def _get_specialization(player_class):
-    print('Please pick one of the following specializations:')
+    print(Fore.CYAN + 'Please choose one of the following specializations: \n'+ Style.RESET_ALL)
     print(*list(class_info.classes[player_class].keys()), sep="   ")
-    specialization = input('Your specialization: ').title()
+    print()
+    specialization = input('Your Specialization: ').title()
+    print()
     while specialization not in list(class_info.classes[player_class].keys()):
-        specialization = input('Please choose a valid specialization:').title()
+        print(Fore.YELLOW + "Please choose a valid specialization. \n" + Style.RESET_ALL)
+        specialization = input('Your Specialization: ').title()
+        print()
     return specialization
 
 def _get_item(player_class, specialization, number):
     item = {}
-    print(f"Please enter stat values for Item {number}: ")
+    print(Fore.CYAN + f"Please enter stat values for Item {number}: \n" + Style.RESET_ALL)
     for k, v in class_info.classes[player_class][specialization].items():
         item[k] = None   
     for k, v in item.items():
@@ -30,9 +39,10 @@ def _get_item(player_class, specialization, number):
             try:
                 item[k] = float(input(str(k).title() + ": "))
             except ValueError:
-                print('Please enter a numerical value.')
+                print(Fore.YELLOW + f'{k.title()} must be a numerical value.' + Style.RESET_ALL)
             else:
                 break
+    print()
     return item
 
 def _calculate_stats(player_class, specialization, item):
@@ -44,11 +54,19 @@ def _calculate_stats(player_class, specialization, item):
 
 def _compare_items(item1_calc, item2_calc):
     if sum(item1_calc.values()) > sum(item2_calc.values()):
-        print("Item 1 provides more output than Item 2")
+        print(Fore.GREEN + "Item 1 provides more output than Item 2 \n" + Style.RESET_ALL)
     if sum(item1_calc.values()) < sum(item2_calc.values()):
-        print("Item 2 provides more output than Item 1")
+        print(Fore.GREEN + "Item 2 provides more output than Item 1 \n" + Style.RESET_ALL)
     if sum(item1_calc.values()) == sum(item2_calc.values()):
-        print("The two items provide the same output")
+        print(Fore.GREEN + "The two items provide the same output \n" + Style.RESET_ALL)
+
+def _restart():
+    restart = input(Fore.CYAN + "Would you like to restart this program? (Y/N) \n" + Style.RESET_ALL)
+    print()
+    if restart == "yes" or restart == "y":
+        main_function()
+    if restart == "n" or restart == "no":
+        print(Fore.GREEN, "Thanks for choosing Dave's Gear Comparison Tool.  Goodbye!" + Style.RESET_ALL)
 
 def main_function():
     player_class = _get_player_class()
@@ -58,20 +76,7 @@ def main_function():
     item1_calc = _calculate_stats(player_class, specialization, item1)
     item2_calc = _calculate_stats(player_class, specialization, item2)
     _compare_items(item1_calc, item2_calc)
+    _restart()
 
-def restart():
-    restart = input("Would you like to restart this program? ")
-    if restart == "yes" or restart == "y":
-        main_function()
-    if restart == "n" or restart == "no":
-        print("Thanks for choosing Dave's Gear Comparison Tool.  Goodbye!")
+main_function()
 
-main_function()  
-restart()
-
-
-
-
-
-
-    
